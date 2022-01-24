8997383bbb9515cea21134996c63f3d445f4ea71
alejorminetti <alejorm@gmail.com>
2022-01-23 23:35:20 -0300

    creando las tablas de la DB postgres mediante los dataframe obtenidos de los CSV guardados

--- AlkemyData.py
+++ AlkemyData.py
@@ -27,12 +27,18 @@ Organizar los archivos en rutas siguiendo la siguiente estructura:
 ○ Si el archivo existe debe reemplazarse. La fecha de la nomenclatura
 es la fecha de descarga.
 """
+from calendar import month
 import requests
 
 import datetime
 import csv
 import os
-import copy
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import pandas as pd
+
 
 def get_data_to_csv(category, url, print_data=False):
     """
@@ -67,32 +73,69 @@ def get_data_to_csv(category, url, print_data=False):
         for item in data['result']['records']:
             writer.writerow(item)
 
-   
-   
-# museos: https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d
-get_data_to_csv('museos', 
-    'https://datos.gob.ar/api/3/action/datastore_search?resource_id=cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d')
+def csv_to_dataframe(category, day, month, year, show_head=False, show_info=False):
+    """
+    read csv file and return dataframe
+    """
+    df = pd.read_csv(category+'/'+str(year)+'-'+str(month)+'/'+category+'-'+str(day)+'-'
+            +str(month)+'-'+str(year)+'.csv')
 
-# salas_cine: https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae
-get_data_to_csv("salas_cine", 
-'https://datos.gob.ar/api/3/action/datastore_search?resource_id=cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae')
+    if show_head:
+        print('First data of: '+category+'-'+str(day)+'-'+str(month)+'-'+str(year)+'.csv'+'\n')
+        print(df.head())
+        print('\n')
 
-# bibliotecas: https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7
-get_data_to_csv('bibliotecas','https://datos.gob.ar/api/3/action/datastore_search?resource_id=cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7')
+    if show_info:
+        print('Columns of: '+category+'-'+str(day)+'-'+str(month)+'-'+str(year)+'.csv'+'\n')
+        print(df.info())
+        print('\n')
+
+    return df
+    """
+    read csv from local directory: ./category/YYYY-MM/category-DD-MM-YYYY.csv
+    and return pandas dataframe
+    """
+
+def create_table(df, category, engine, show_query=False):
+    """
+    create table in database
+    """
+    df.to_sql(category, engine, if_exists='replace', index=False)
+
+    if show_query:
+        print(category+' table created, show query of 3 rows:')
+        for query in engine.execute('SELECT * FROM '+category+' LIMIT 3'):
+            print(query)
+        print('\n')
 
 
-"""
-pip install SQLAlchemy
-pip install psycopg2
-"""
+if __name__ == '__main__':
+    ## save data from url to csv:
 
-"""
-# SQLAlchemy for connecting to a postgreSQL database
-from sqlalchemy import create_engine
-engine = create_engine('postgresql+psycopg2://user:password@hostname/database_name')
+    # museos: https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d
+    get_data_to_csv('museos', 
+        'https://datos.gob.ar/api/3/action/datastore_search?resource_id=cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d')
 
-# sin usar SQLAlchemy
-import psycopg2
-conn_string = "host='localhost' dbname='my_database' user='postgres' password='secret'"
-conn = psycopg2.connect(conn_string)
-"""
+    # salas_cine: https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae
+    get_data_to_csv("salas_cine", 
+    'https://datos.gob.ar/api/3/action/datastore_search?resource_id=cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae')
+
+    # bibliotecas: https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7
+    get_data_to_csv('bibliotecas','https://datos.gob.ar/api/3/action/datastore_search?resource_id=cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7')
+
+    ## read csv to dataframe:
+    day = datetime.datetime.now().day
+    month = datetime.datetime.now().month
+    year = datetime.datetime.now().year
+    df_museos = csv_to_dataframe('museos', day, month, year, show_head=True, show_info=True)
+    df_salas_cine = csv_to_dataframe('salas_cine', day, month, year, show_head=True, show_info=True)
+    df_bibliotecas = csv_to_dataframe('bibliotecas', day, month, year, show_head=True, show_info=True)
+    
+    # connect to postgreSQL database using sqlalchemy
+    engine = create_engine('postgresql+psycopg2://postgres:your_password@localhost:5433/prueba')
+    
+    # create tables on postgreSQL database from df_museos, df_salas_cine and df_bibliotecas
+    create_table(df_museos, 'museos', engine, show_query=True)
+    create_table(df_salas_cine, 'salas_cine', engine, show_query=True)
+    create_table(df_bibliotecas, 'bibliotecas', engine, show_query=True)
+  
